285b2aa31ad232d3bef0a8060dda976bc614257c
zhangby <zhangby@tenzont.com>
2020-04-01 17:06:33 +0800

    注册中心集群配置

--- src/main/java/com/zby/eureka/server/SysApplication.java
+++ src/main/java/com/zby/eureka/server/SysApplication.java
@@ -2,6 +2,7 @@ package com.zby.eureka.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
@@ -10,7 +11,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @EnableEurekaServer
 @SpringBootApplication
-
+@EnableEurekaClient
 public class SysApplication {
     public static void main(String[] args) {
         SpringApplication.run(SysApplication.class,args);
